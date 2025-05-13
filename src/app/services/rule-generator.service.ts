35c85a46a3b76e40ca8b4abf66eecfdb48fc12ff
Alejandro Hierro <alejandrohierro@gmail.com>
2025-05-13 17:57:34 -0300

    fix

--- src/app/services/rule-generator.service.ts
+++ src/app/services/rule-generator.service.ts
@@ -1,35 +1,16 @@
-import {Injectable} from '@angular/core';
-import {ExpectedResults} from "../model/expectedResults";
-import {Rule} from "../model/rule";
-import {RuleCondition} from "../model/rule-condition";
+import { Injectable } from '@angular/core';
+import { ExpectedResults } from "../model/expectedResults";
+import { Rule } from "../model/rule";
+import { RuleCondition } from "../model/rule-condition";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RuleGeneratorService {
 
-  aliases = new Map();
   fieldNames = new Map();
 
   constructor() {
-
-    this.aliases.set("payerId", "$payer");
-    this.aliases.set("recoPayerId", "$recoPayerId");
-    this.aliases.set("revCode", "$revCode");
-    this.aliases.set("providerType", "$providerType");
-    this.aliases.set("patientType", "$pType");
-    this.aliases.set("providerId", "$provider");
-    this.aliases.set("medicalService", "$mService");
-    this.aliases.set("providerCode", "$providerCode");
-    this.aliases.set("procedureCode", "$procCode");
-    this.aliases.set("iiis", "$list");
-    this.aliases.set("bnftCvgeLevelCode", "$bnftCvgeLevelCode");
-    this.aliases.set("refId", "$refId");
-    this.aliases.set("message", "$msg");
-    this.aliases.set("inNetworkInd", "$inNetwork");
-    this.aliases.set("customerAbbr", "$custAbbr");
-    this.aliases.set("groupNum", "$groupNum");
-    this.aliases.set("memberNum", "$memNum");
 
     this.fieldNames.set("procedureCode", `dalDelivery["ProcedureCode"]`);
     this.fieldNames.set("iiis", `this["III02List"]`);
@@ -49,8 +30,8 @@ export class RuleGeneratorService {
     if (fieldValue.includes(",") && fieldValue.length > 3) {
       return (negate) ? " not in " : " in " + "(" + fieldValue.split(",")
         .map(fieldVal => this.formatSingleValue(fieldVal, isNumber))
-        .sort((b,a) => {
-          if (typeof b == 'number' && typeof a == 'number' ) {
+        .sort((b, a) => {
+          if (typeof b == 'number' && typeof a == 'number') {
             return b - a;
           } else {
             if (a > b) {
@@ -78,33 +59,32 @@ export class RuleGeneratorService {
 
   getCommonCondition(fieldName: string, fieldValue: any, isNumber: boolean, negate: boolean) {
     const formattedVal = this.formatFieldValue(fieldValue, isNumber, negate);
-    const alias = this.aliases.get(fieldName);
-    return ` ${alias} : ${this.getFieldName(fieldName)}${formattedVal}`
+    return ` ${this.getFieldName(fieldName)}${formattedVal}`
   }
-  getFieldList( ruleConditions: RuleCondition[],conditionType:string){
-    let stcResolutionInput:string[] = [];
-    if(ruleConditions && ruleConditions.length){
+  getFieldList(ruleConditions: RuleCondition[], conditionType: string) {
+    let stcResolutionInput: string[] = [];
+    if (ruleConditions && ruleConditions.length) {
       stcResolutionInput = ruleConditions.filter(ruleCond => ruleCond.conditionType == conditionType)
-        .map(ruleCond => this.getCommonCondition(ruleCond.fieldName,ruleCond.value,ruleCond.isNumber,ruleCond.negate))
+        .map(ruleCond => this.getCommonCondition(ruleCond.fieldName, ruleCond.value, ruleCond.isNumber, ruleCond.negate))
 
     }
     const sep = `,
           `;
     const init = `
           `;
-    return this.concatSepAtTheBeginning(stcResolutionInput.join(sep),init);
+    return this.concatSepAtTheBeginning(stcResolutionInput.join(sep), init);
   }
-  concatSepAtTheBeginning(str:string, sep:string){
-    return str?`${sep}${str}`:'';
+  concatSepAtTheBeginning(str: string, sep: string) {
+    return str ? `${sep}${str}` : '';
   }
-  concatSepAtTheEnd(str:string, sep:string){
-    return str?`${str}${sep}`:'';
+  concatSepAtTheEnd(str: string, sep: string) {
+    return str ? `${str}${sep}` : '';
   }
   generateRule(rule: Rule): string {
-    const stcResolutionInputStr = this.getFieldList(rule.ruleConditions,"StcResolutionInput");
-    const episode = this.concatSepAtTheEnd(this.concatSepAtTheBeginning(this.getFieldList(rule.ruleConditions,"episode"),'\n        DalMap('),'\n        ) from $episode\n');
+    const stcResolutionInputStr = this.getFieldList(rule.ruleConditions, "StcResolutionInput");
+    const episode = this.concatSepAtTheEnd(this.concatSepAtTheBeginning(this.getFieldList(rule.ruleConditions, "episode"), '\n        DalMap('), '\n        ) from $episode\n');
 
-    const benefitStr = this.concatSepAtTheEnd(this.concatSepAtTheBeginning(this.getFieldList(rule.ruleConditions,"Benefit"),','),',')  || ',';
+    const benefitStr = this.concatSepAtTheEnd(this.concatSepAtTheBeginning(this.getFieldList(rule.ruleConditions, "Benefit"), ','), ',') || ',';
     const stc = rule.ruleConditions.find(ruleCond => ruleCond.fieldName == 'stc');
     const message = rule.ruleConditions.find(ruleCond => ruleCond.fieldName == 'message');
     const setMsg = this.getSetMsg(message);
@@ -112,7 +92,7 @@ export class RuleGeneratorService {
     const procCode = rule.ruleConditions.find(ruleCond => ruleCond.fieldName == 'procedureCode');
 
     let delivery = '';
-    if(procCode){
+    if (procCode) {
       delivery = `
            $delivery : dalDelivery != null,`
     }
@@ -125,10 +105,9 @@ rule "${rule.name}"
         )
         ${planDesc} ${episode}
         $benefit : DalMap(
-           $stc : this["SvcTypeCode"] == "${stc?.value}",
-           $bic : this["BnftInfoCode"] in (BenefitTypeCode.CoInsurance.code, BenefitTypeCode.CoPayment.code)${benefitStr}
-           $copay : this["BnftAmt"] != null ||
-           $coins : this["BnftPercent"] != null
+           this["SvcTypeCode"] == "${stc?.value}",
+           this["BnftInfoCode"] in (BenefitTypeCode.CoInsurance.code, BenefitTypeCode.CoPayment.code)${benefitStr}
+           this["BnftAmt"] != null || this["BnftPercent"] != null
         ) from $benefits
 
     then
@@ -140,20 +119,35 @@ end`
   generateTest(expectedResults: ExpectedResults): string {
     const ded = this.getDed(expectedResults.ded);
     const stc = this.getStc(expectedResults.stc);
-
+    const copay = this.getCopay(expectedResults.copay);
+    const coins = this.getCoins(expectedResults.coins);
     return `public IRunnableTest test${expectedResults.ticketNumber}(){
   RunnableTest test = new RunnableJsonTest();
-  test.setTestId("${expectedResults.ticketNumber}");
-  test.assertAllCopay(${expectedResults.copay});
-  test.assertAllCoinsurance(${expectedResults.coins});${ded} ${stc}
+  test.setTestId("${expectedResults.ticketNumber}");${copay} ${coins} ${ded} ${stc}
   return test;
 }`;
+  }
+  getCoins(coins: string) {
+    if (!!coins) {
+      return `
+  test.assertAllCoinsurance(${coins});`;
+    } else{
+      return '';
+    }
+  }
+  private getCopay(copay: string) {
+    if (!!copay) {
+      return `
+  test.assertAllCopay(${copay});`;
+    } else{
+      return '';
+    }
   }
   private getStc(stc: string) {
     if (!!stc) {
       return `
   test.assertAllStc("${stc}");`;
-    }else{
+    } else {
       return '';
     }
   }
@@ -171,7 +165,7 @@ end`
   }
 
   private getSetMsg(ruleCondition: RuleCondition | undefined) {
-    if(ruleCondition==undefined){
+    if (ruleCondition == undefined) {
       return '';
     }
     return `
@@ -179,14 +173,14 @@ end`
   }
 
   private getPlanDesc(ruleCondition: RuleCondition | undefined) {
-    if(ruleCondition==undefined){
+    if (ruleCondition == undefined) {
       return '';
     }
     return `
         DalMap(
             this["SvcTypeCode"] == "30",
             this["BnftInfoCode"] == BenefitTypeCode.ActiveCoverage.code,
-            $planDesc : this["PlanCvgeDesc"] == "${ruleCondition.value}"
+            this["PlanCvgeDesc"] == "${ruleCondition.value}"
         ) from $benefits
 `;
   }
